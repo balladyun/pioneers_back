ed3366e707775687350aecc4eef3a2987a1d5011
horang <fnelclsrn123@gmail,com>
2024-08-06 13:17:26 +0900

    chore: git 폴더 구조 변경

--- pioneers/src/main/java/com/pioneers/repository/MbtiRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pioneers.repository;
-
-import com.pioneers.entity.MbtiResult;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface MbtiRepository extends JpaRepository<MbtiResult, Long> {
-}
