a934aa282a996b1b714e481afe921d68b6cb21f2
sxvxnxwt <159557931+sxvxnxwt@users.noreply.github.com>
2024-08-06 12:19:21 +0900

    Add files via upload

---
+++ pioneers/src/main/java/com/example/pioneers/entity/MbtiResult.java
@@ -0,0 +1,20 @@
+package com.example.pioneers.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "mbti")
+public class MbtiResult {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String result;
+
+    public MbtiResult(Long id, String result){
+        this.id = id;
+        this.result = result;
+    }
+}
