e80f277b91d155a88ca9a47547891397dca4026b
horang <fnelclsrn123@gmail,com>
2024-08-13 14:44:40 +0900

    refactor: 배포 리액트 서버 cors 허용

--- src/main/java/com/pioneers/config/WebConfig.java
+++ src/main/java/com/pioneers/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://pioneers-blush.vercel.app/")
                 .allowedMethods("GET", "POST")
                 .maxAge(3000);
     }
